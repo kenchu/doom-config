726ac3aea5ea33f8da85976f049f4170abd741a0
Ken Chu <chuwingyu@gmail.com>
2020-11-13 03:07:51 +0800

    Add magit +forge flag.

--- init.el
+++ init.el
@@ -99,7 +99,7 @@
        (lookup +docsets
                +dictionary) ; navigate your code and its documentation
        (lsp +peek)
-       magit             ; a git porcelain for Emacs
+       (magit +forge)    ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
